d6a9864a2a5ee7104a10eb162f1c6f1b3c5e92c8
Matheus Hentges <matheus-hentges@live.com>
2021-05-17 13:46:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -33,9 +33,9 @@ error_reporting(E_ALL);
 	if(isset($_GET['media'])) {
 		$media = $_GET['media'];
 		if ($media >= 10) {
-			echo '<h2 class="verde"> A média é ' . number_format($media, 2, '.', ',') . '</h2>';
+			echo '<h2 class="verde"> A média é ' . number_format($media, 2, ',', ',') . '</h2>';
 		}else{
-			echo '<h2 class="vermelho"> A média é ' . number_format($media, 2, '.', ',') . '</h2>';
+			echo '<h2 class="vermelho"> A média é ' . number_format($media, 2, ',', ',') . '</h2>';
 		}
 	}
 ?>
